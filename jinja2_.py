b27ecb4f749bec3e6cafc392359b76543efddb85
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-03-11 17:09:49 +0900

    jinja2

--- jinja2_.py
+++ jinja2_.py
@@ -1,15 +1,15 @@
 from jinja2 import Environment
 
-template = Environment().from_string("""{% for n in range(10) %}
-{{% if n % 15 == 0 %}}
-FizzBuzz"
-{{% elif n % 3 == 0 %}}
+template = Environment().from_string("""{%- for n in range(1, 101) -%}
+{%- if n % 15 == 0 %}
+FizzBuzz
+{%- elif n % 3 == 0 %}
 Fizz
-{{% elif n % 5 == 0 %}}
+{%- elif n % 5 == 0 %}
 Buzz
-{{% else %}}
-{{n}}
-{{% endif %}}
-{% endfor -%}""")
+{%- else %}
+{{ n }}
+{%- endif %}
+{%- endfor -%}""")
 
 print(template.render())
